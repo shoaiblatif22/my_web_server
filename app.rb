a3f67e9d138c80d69831c020330e8e7e658c8d5b
Shoaib Latif <shoaiblatif11@gmail.com>
2023-05-30 15:30:18 +0100

    Updated app.rb

--- app.rb
+++ app.rb
@@ -2,4 +2,15 @@ require 'sinatra/base'
 
 class Application < Sinatra::Base
 
+  get '/' do
+    return 'Hello!'
+  end
+
+  get '/posts' do
+    return 'A list of posts'
+  end
+
+  post '/posts' do
+    return 'A post was created'
+  end
 end
